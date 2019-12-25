0517650775e9a0acb76927a5a98e488414a941a7
Simon 'Firesphere' Erkelens <github@casa-laguna.net>
2019-12-26 12:17:14 +1300

    CS Fixes

--- tests/unit/FluentSiteStateTest.php
+++ tests/unit/FluentSiteStateTest.php
@@ -3,14 +3,12 @@
 
 namespace Firesphere\SolrFluent\Tests;
 
-
 use Firesphere\SolrFluent\States\FluentSiteState;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Dev\SapphireTest;
 
 class FluentSiteStateTest extends SapphireTest
 {
-
     public function testAppliesTo()
     {
         $state = new FluentSiteState();
