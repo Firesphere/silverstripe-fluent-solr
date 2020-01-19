503c0b580ec908971d5efbc79e9fb291cda83d17
Simon 'Firesphere' Erkelens <github@casa-laguna.net>
2020-01-20 10:51:53 +1300

    Index extension test

---
+++ tests/unit/FluentIndexExtensionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Firesphere\SolrFluent\Tests;
+
+
+use Firesphere\SolrFluent\Extensions\FluentIndexExtension;
+use Firesphere\SolrSearch\Queries\BaseQuery;
+use Firesphere\SolrSearch\States\SiteState;
+use SilverStripe\Dev\SapphireTest;
+use Solarium\QueryType\Select\Query\Query;
+use TractorCow\Fluent\Model\Locale;
+use TractorCow\Fluent\State\FluentState;
+
+class FluentIndexExtensionTest extends SapphireTest
+{
+    protected static $fixture_file = '../fixtures/FluentTest.yml';
+
+    public function testOnBeforeInit()
+    {
+        $extension = new FluentIndexExtension();
+
+        $extension->onBeforeInit();
+
+        $this->assertEquals(Locale::get()->count(), SiteState::getStates());
+    }
+
+    public function testOnBeforeSearch()
+    {
+        FluentState::singleton()->setLocale('en_NZ');
+        $query = new BaseQuery();
+        $clientQuery = new Query();
+        $index = new \CircleCITestIndex();
+
+        $extension = new FluentIndexExtension();
+
+        $extension->setOwner($index);
+
+        $extension->onBeforeSearch($query, $clientQuery);
+
+        $field = $clientQuery->getQueryDefaultField();
+
+        $this->assertEquals('en_NZ_text', $field);
+    }
+}
